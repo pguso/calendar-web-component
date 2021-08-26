70baa2ba019ff1b15b7973287f7489782a9fbc42
Patric Robert Gutersohn <gutersohn-extern@gvl.de>
2021-08-26 11:47:21 +0200

    remove unused async wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 import 'core-js/stable'
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter'
-// @ts-ignore
 import '@webcomponents/webcomponentsjs/webcomponents-loader'
-
-// @ts-ignore
-WebComponents.waitFor(() => {
-    return import('./components/index')
-})
+import './components/index'
